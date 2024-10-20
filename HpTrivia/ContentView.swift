3908d1fe2b30fcfa935029b31be600e4e5972bb4
pallavijoshi <pallavijoshi4@gmail.com>
2024-10-20 17:49:46 +0200

    add more animtaion

--- HpTrivia/ContentView.swift
+++ HpTrivia/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var scalePlayButton = false
     @State private var moveBackgroundImage = false
+    @State private var animateViewsIn = false
     
     var body: some View {
         GeometryReader { geo in
@@ -36,86 +37,118 @@ struct ContentView: View {
                     }
                 
                 VStack {
-                    
                     VStack {
-                        Image(systemName: "bolt.fill")
-                            .font(.largeTitle)
-                            .imageScale(.large)
-                        
-                        Text("HP")
-                            .font(.custom(Constants.HPfont, size: 70))
-                        
-                        Text("Trivia")
-                            .font(.custom(Constants.HPfont, size: 60))
-                        
+                        if (animateViewsIn) {
+                            VStack {
+                                Image(systemName: "bolt.fill")
+                                    .font(.largeTitle)
+                                    .imageScale(.large)
+                                
+                                Text("HP")
+                                    .font(.custom(Constants.HPfont, size: 70))
+                                
+                                Text("Trivia")
+                                    .font(.custom(Constants.HPfont, size: 60))
+                                
+                            }
+                            .padding(.top, 70)
+                            .transition(.move(edge: .top))
+                        }
                     }
-                    .padding(.top, 70)
+                    .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
                     
                     Spacer()
                     
                     VStack {
-                        Text("Recent Scores")
-                            .font(.title2)
-                        Text("200")
-                        Text("130")
-                        Text("140")
+                        if (animateViewsIn) {
+                            VStack {
+                                Text("Recent Scores")
+                                    .font(.title2)
+                                Text("200")
+                                Text("130")
+                                Text("140")
+                            }
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                            .background(.black.opacity(0.7))
+                            .cornerRadius(15)
+                            .transition(.opacity)
+                        }
                     }
-                    .font(.title3)
-                    .foregroundColor(.white)
-                    .padding(.horizontal)
-                    .background(.black.opacity(0.7))
-                    .cornerRadius(15)
-                    
+                    .animation(.linear(duration: 1).delay(4), value: animateViewsIn)
+
                     Spacer()
                     
                     HStack {
                         Spacer()
-                        Image(systemName: "info.circle.fill")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                            .shadow(radius: 5)
-                        
-                        Spacer()
-                        
-                        Button(
-                            action: {
-                                
-                            },
-                            label: {
-                                Text("Play")
+                        VStack {
+                            if (animateViewsIn) {
+                                Image(systemName: "info.circle.fill")
                                     .font(.largeTitle)
                                     .foregroundColor(.white)
-                                    .padding(.vertical, 7)
-                                    .padding(.horizontal, 50)
-                                    .background(.brown)
-                                    .cornerRadius(7)
                                     .shadow(radius: 5)
-                                    .scaleEffect(scalePlayButton ? 1.2 : 1)
-                                    .onAppear {
-                                        withAnimation(
-                                            .easeInOut(duration: 1.3)
-                                            .repeatForever()) {
-                                                scalePlayButton.toggle()
-                                            }
-                                    }
-                                
+                                    .transition(.offset(x: -geo.size.height / 4))
                             }
-                            
-                        )
-                        
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewsIn)
+
                         
                         Spacer()
                         
-                        Button(
-                            action: {
-                                
-                            },
-                            label: {
-                                Image(systemName: "gearshape.fill")
-                                    .font(.largeTitle)
-                                    .foregroundColor(.white)
-                                    .shadow(radius: 5)
-                            })
+                        VStack {
+                            if (animateViewsIn) {
+                                Button(
+                                    action: {
+                                        
+                                    },
+                                    label: {
+                                        Text("Play")
+                                            .font(.largeTitle)
+                                            .foregroundColor(.white)
+                                            .padding(.vertical, 7)
+                                            .padding(.horizontal, 50)
+                                            .background(.brown)
+                                            .cornerRadius(7)
+                                            .shadow(radius: 5)
+                                            .scaleEffect(scalePlayButton ? 1.2 : 1)
+                                            .onAppear {
+                                                withAnimation(
+                                                    .easeInOut(duration: 1.3)
+                                                    .repeatForever()) {
+                                                        scalePlayButton.toggle()
+                                                    }
+                                            }
+                                        
+                                    }
+                                )
+                                .transition(.offset(y: geo.size.height / 3))
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
+
+                        
+                        Spacer()
+                        
+                        VStack {
+                            if (animateViewsIn) {
+                                Button(
+                                    action: {
+                                        
+                                    },
+                                    label: {
+                                        Image(systemName: "gearshape.fill")
+                                            .font(.largeTitle)
+                                            .foregroundColor(.white)
+                                            .shadow(radius: 5)
+                                    }
+                                )
+                                .transition(.offset(x: geo.size.width / 4))
+
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewsIn)
+
                         Spacer()
                         
                     }
@@ -131,7 +164,8 @@ struct ContentView: View {
         }
         .ignoresSafeArea()
         .onAppear {
-            playAudio()
+            animateViewsIn = true
+            //playAudio()
         }
     }
     
@@ -139,7 +173,7 @@ struct ContentView: View {
         let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
         audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
         audioPlayer.numberOfLoops = -1
-       // audioPlayer.play()
+        audioPlayer.play()
     }
 }
 
