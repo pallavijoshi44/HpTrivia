3469fcbac6485b43537a965eba0d3f2b879a09fe
pallavijoshi <pallavijoshi4@gmail.com>
2024-10-20 12:03:33 +0200

    Initial Commit

---
+++ HpTrivia/HpTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HpTriviaApp.swift
+//  HpTrivia
+//
+//  Created by Pallavi Joshi on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HpTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
